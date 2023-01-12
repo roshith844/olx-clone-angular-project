143591f8e3b4f7279f252d1aae8a0cd9207db2e7
Roshith P <roshith844@gmail.com>
2023-01-12 19:28:56 +0530

    fix: posts

--- project/src/app/components/recommendations/recommendations.component.ts
+++ project/src/app/components/recommendations/recommendations.component.ts
@@ -15,27 +15,27 @@ export class RecommendationsComponent {
 
     },
     {
-         name: "porsche Macan R4",
-         image: "https://apollo-singapore.akamaized.net/v1/files/59fj6n9p8k073-IN/image;s=780x0;q=60",
-         price: 774000,
-         model: "2020",
-         location: "delhi",
+         name: "Toyotta ennov",
+         image: "https://apollo-singapore.akamaized.net/v1/files/rhn0nv12w60t-IN/image;s=780x0;q=60",
+         price: 804000,
+         model: "2012",
+         location: "kerala",
 
     },
     {
-         name: "porsche Macan R4",
-         image: "https://apollo-singapore.akamaized.net/v1/files/59fj6n9p8k073-IN/image;s=780x0;q=60",
-         price: 774000,
-         model: "2020",
-         location: "delhi",
+         name: "Air Conditioner",
+         image: "https://apollo-singapore.akamaized.net/v1/files/crou3ct2lzvy2-IN/image;s=780x0;q=60",
+         price: 4000,
+         model: "1999",
+         location: "TamilNadu",
 
     },
     {
-         name: "porsche Macan R4",
-         image: "https://apollo-singapore.akamaized.net/v1/files/59fj6n9p8k073-IN/image;s=780x0;q=60",
-         price: 774000,
-         model: "2020",
-         location: "delhi",
+         name: "TV",
+         image: "https://apollo-singapore.akamaized.net/v1/files/klrxl3jtdmdj1-IN/image;s=780x0;q=60",
+         price: 7000,
+         model: "2000",
+         location: "karnataka",
 
     },
     {
